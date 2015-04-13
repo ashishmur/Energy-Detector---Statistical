4c1111e57631fb4adfc6a781bab0aef73efbe9bb
ashishmur <ashishmur@gmail.com>
2015-04-13 19:37:37 +0530

    Update Static Detection.m
    The following updated file is used to find the Pd, Pfa, Pm, Pr for any given signal based on the provided standard deviation coefficient.
    The editable variables are in the initial lines of code. They are:
    1. L - the maximum frequency to be scanned
    2.fs_temp - the sampling frequency of the transmitted signal. Ensure to keep it 2*L.
    3.SNR - the signal to noise ratio.
    4.fs - sampling frequency of the energy detector.
    5. nfft - the n point fft, Keep it greater than 2*fs.
    6. k - the standard deviation coefficient.

--- Static Detection.m
+++ Static Detection.m
@@ -1,31 +1,32 @@
 clear all;
 close all;
 clc;
-
+L=2000000;                                                                  %Maximum frequency to scan.
+fs_temp=4000000;                                                            %Sampling rate of the transmitted singal. Ensure atleast 2*L
+snr=10;                                                                     %Signal to noise ratio of AWGN Channel.
+fs=1024;                                                                    %Sampling rate at cognitive radio / Energy Detector.
+nfft=2048;                                                                  %Number of FFT points. Ensure it is atleast 2*fs
+k=-0.03;                                                                    % Value of the standard deviation coefficient. Takes values from -1 t0 1.
 %step 1 is to generate a signal
-L=6000
-fc1=2000;
-fc2=3000;
-fs=12000;
+fc1=input('enter lower bandwidth of transmitted signal \n');                % lower bandwidth of the signal
+fc2=input('enter upper bandwidth of transmitted signal \n');                % Upper Bandwidth of the Signal
+
 	x=randn(1,L);
 	b=1;a=1;
-	if fc1==0 & fc2 < .5*fs
-		[b,a]=butter(8,(2/fs)*fc2);
+	if fc1==0 & fc2 < .5*fs_temp
+		[b,a]=butter(8,(2/fs_temp)*fc2);
 	end
-	if fc1>0 & fc2<.5*fs
-		[b,a]=butter(4,(2/fs)*[fc1,fc2]);
+	if fc1>0 & fc2<.5*fs_temp
+		[b,a]=butter(4,(2/fs_temp)*[fc1,fc2]);
 	end
-	if fc1>0 & fc2 >.49*fs
-		[b,a]=butter(4,(2/fs)*fc1,'high');
+	if fc1>0 & fc2 >.49*fs_temp
+		[b,a]=butter(4,(2/fs_temp)*fc1,'high');
 	end		
-	y=filter(b,a,x);
-    
+	tx=filter(b,a,x);                                                       % tx is the transmitted signal.
     %step 2 adding awgn noise
-    y=awgn(y,35,'measured');
+    y=awgn(tx,snr,'measured');
     
     % step 3 finding fft
-    
-    nfft=12000;
     Y=fft(y,nfft);
     Y=Y(1:nfft/2);
     mx=abs(Y);
@@ -33,7 +34,9 @@ fs=12000;
     figure(1),plot(f,mx);
     
     %step 4 threshold detection
-    
+ for i=1:(nfft/2)
+    mx(i)=power(mx(i),2);
+end   
 sum=0;
 for i=1:(nfft/2)
     sum=sum+mx(i);
@@ -49,7 +52,6 @@ end
 var=var/(nfft/2);
 sigma=sqrt(var);
 const=zeros(1,(nfft/2));
-k=0;
 th=mean+(k*sigma);
 for i=1:(nfft/2)
 if (mx(i)<th)
@@ -62,32 +64,38 @@ figure(2),plot(f,const);
 %step 5 calculating efficiency
 flag_1=0;
 test=zeros(1,(nfft/2));
-for i=fc1:fc2
+res=(L/(nfft/2));
+beg=int32(fc1/res)+1;
+last=int32(fc2/res)+1;
+for i=beg:last
     test(i)=1;
-    flag_1=flag_1+1;
+    flag_1=flag_1 +1;
 end
 flag_0=(nfft/2)-flag_1;
 
 count_D=0;
 count_F=0;
+count_R=0;
+count_M=0;
 for i=1:(nfft/2)
     if const(i)==1 && test(i)==1
-        count_D=count_D +1;
+        count_D=count_D+1;
     else if test(i)==0 && const(i)==1
             count_F=count_F+1;
         end
     end
 end
 
+for i=1:(nfft/2)
+    if const(i)==0 && test(i)==0
+        count_R=count_R+1;
+    else if test(i)==1 && const(i)==0
+            count_M=count_M+1;
+        end
+    end
+end
 
-Pd=(count_D/flag_1)*100;
-Pf=(count_F/flag_0)*100;
-
-
-
-
-
-
-    
-
-
+Pd=(count_D/flag_1)
+Pf=(count_F/flag_0)
+Pm=(count_M/flag_1)
+Pr=(count_R/flag_0)
